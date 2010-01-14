0f6f3eed00f13e07dc43ff948fde5218866cf579
Byrne Reese <byrne@majordojo.com>
2010-01-13 16:18:33 -0800

    Major enhancements and moving plugin to 1.0 status. Enhancements will:
    * display the short URL below the entry's title
    * allow users to repost
    * only tweet once
    * allow URLs to be copied to clipboard

--- plugins/Supr/lib/Supr/Callbacks.pm
+++ plugins/Supr/lib/Supr/Callbacks.pm
@@ -1,7 +1,7 @@
 package Supr::Callbacks;
 use strict;
 
-use MT::Util qw ( trim remove_html );
+use MT::Util qw ( trim remove_html relative_date );
 
 sub entry_pre_save {
     my ( $cb, $entry, $entry_orig ) = @_;
@@ -13,19 +13,19 @@ sub entry_pre_save {
     my $entry_id = $entry->id;
     my $supr_it  = 1;
 
-    if ( $entry->status == 2 ) {
+    if ( $entry->status == MT->model('entry')->RELEASE() ) {
         if ( !$entry_id ) {
             $supr_it = 1;          # new entry with published status
         }
         else {
-
-# entry was previously saved in db -- now determine if it status has just been changed to published
+            # entry was previously saved in db -- now determine if it status has 
+            # just been changed to published
             $entry->clear_cache();
             $entry->uncache_object();
             $entry_orig = MT->model('entry')->load($entry_id);
-            if ( $entry_orig->status != 2 ) {
-
-# now we know status has just been changed to published and we have no status_id on record - so supr it
+            if ( $entry_orig->status != MT->model('entry')->RELEASE() ) {
+                # now we know status has just been changed to published and we have 
+                # no status_id on record - so supr it
                 $supr_it = 1;
             }
         }
@@ -35,8 +35,7 @@ sub entry_pre_save {
         MT->log(
             {
                 blog_id => $entry->blog_id,
-                message => $entry->title
-                  . " just published and should be supred"
+                message => $entry->title . " just published and should be supred"
             }
         );
         $entry->{supr_it} = 'yes';
@@ -46,16 +45,23 @@ sub entry_pre_save {
 
 sub entry_post_save {
     my ( $cb, $entry, $entry_orig ) = @_;
-    return if $entry->supr_url;    # alreaded supred
-    return unless $entry->{supr_it};
-    my $entry_id = $entry->id;
+    require MT::Request;
+    my $r = MT::Request->instance();
+    return if $r->stash('supred_already_this_session');
+
+    my $app = MT->app;
+    my $q = $app->param;
     my $plugin   = MT->component('Supr');
     my $config   = $plugin->get_config_hash( 'blog:' . $entry->blog_id );
     my $enabled  = $config->{supr_enable};
     return if !$enabled;
+
     my $supr_username = $config->{supr_username};
     my $supr_apikey   = $config->{supr_apikey};
     return unless ( $supr_username && $supr_apikey );
+
+    return if $entry->supr_url && !$q->param('supr_repost');    # alreaded supred
+    return unless $entry->{supr_it} || $q->param('supr_repost');
 
     if ( $entry->authored_on =~
         m!(\d{4})-(\d{2})-(\d{2})\s+(\d{2}):(\d{2})(?::(\d{2}))?! )
@@ -64,48 +70,38 @@ sub entry_post_save {
         my $ts = sprintf "%04d%02d%02d%02d%02d%02d", $1, $2, $3, $4, $5, $s;
         $entry->authored_on($ts);
     }
-    my $tweet_text;
-    my $twitter_this;
-    my $fb_this;
-
-    my $app = MT->app;
+    my ($tweet_text,$twitter_this,$fb_this,@services);
     if ( $app->can('param') ) {
-        my $q = $app->param;
         $tweet_text = remove_html( $q->param('su_twitter') )
           if $q->param('su_twitter');
-        $twitter_this = $q->param('twitter-this') if $q->param('twitter-this');
-        $fb_this      = $q->param('fb-this')      if $q->param('fb-this');
+        push @services, 'twitter' if $q->param('twitter-this');
+        push @services, 'facebook' if $q->param('fb-this');
     }
-
-    my $field = 'title';
-
-    $tweet_text = remove_html( $entry->$field ) unless $tweet_text;
-
     return if !$tweet_text;
 
-    my $tweet = $tweet_text;
     my $url   = $entry->permalink;
-    $tweet .= ' ' . $url;
+    if ($q->param('supr_repost')) {
+	MT->log("Tweet text BEFORE: $tweet_text");
+	$tweet_text = _str_replace($entry->supr_url,$url,$tweet_text);
+	MT->log("Tweet text AFTER: $tweet_text");
+    } else {
+	$tweet_text .= ' ' . $url;
+    }
 
     require WWW::Shorten::Supr;
     my $supr =
       WWW::Shorten::Supr->new( USER => $supr_username, APIKEY => $supr_apikey );
-    my @services;
-    push @services, 'twitter'  if ($twitter_this);
-    push @services, 'facebook' if ($fb_this);
-    my $suprurl;
-    my $suprmsg;
 
+    my ($supr_url,$supr_msg);
     if (@services) {
-
         # one more services was requested so use 'post' API method
-        $suprmsg = $supr->post( msg => $tweet, services => \@services );
-        $suprurl = extract_suprurl($suprmsg);
+	MT->log("Posting $tweet_text");
+        $supr_msg = $supr->post( msg => $tweet_text, services => \@services );
+        $supr_url = extract_suprurl($supr_msg);
     }
     else {
-
         # no posting requested, but get su.pr url using 'shorten' API method
-        $suprurl = $supr->shorten( URL => $url );
+        $supr_url = $supr->shorten( URL => $url );
     }
     if ( $supr->is_error ) {
         MT->log(
@@ -116,16 +112,23 @@ sub entry_post_save {
                   . $supr->error_message
             }
         );
-        $entry->{supred} = 0;
+        $entry->{supred_already_this_session} = 0;
         return 1;
     }
 
-    if ($suprurl) {
-        $entry->supr_url($suprurl);
-        $entry->supr_text($tweet);
+    if ($supr_url) {
+        $entry->supr_url($supr_url);
+        $entry->supr_text($supr_msg);
+	$entry->supr_posted_to(join(',',@services));
+
+        my @ts = MT::Util::offset_time_list(time, $entry->blog_id);
+        my $ts = sprintf '%04d%02d%02d%02d%02d%02d',
+	$ts[5]+1900, $ts[4]+1, @ts[3,2,1,0];
+        $entry->supr_posted_on($ts);
+
+	$r->stash('supred_already_this_session',1);
         $entry->save;
     }
-    $entry->{supred} = 1;
 
     return 1;
 }
@@ -144,26 +147,38 @@ sub edit_entry_xfrm {
     my $slug;
     $slug = <<END_TMPL;
 <link rel="stylesheet" type="text/css" href="<mt:StaticWebPath>plugins/Supr/app.css" />
+<script type="text/javascript" src="<mt:StaticWebPath>jquery/jquery.js"></script>
+<script type="text/javascript" src="<mt:StaticWebPath>plugins/Supr/jquery.clipboard.min.js"></script>
 END_TMPL
-    $$tmpl =~ s{(<mt:setvarblock name="html_head" append="1">)}{$1 $slug}msgi;
+    $$tmpl =~ s{(<mt:setvarblock name="html_head" append="1">)}{$1 $slug}msi;
 }
 
 sub edit_entry_param {
     my ( $cb, $app, $param, $tmpl ) = @_;
 
+    my $blog          = $app->blog;
     my $q             = $app->param;
-    my $entry_blog_id = $q->param('blog_id');
     my $author        = $app->user;
     my $plugin        = MT->component('Supr');
-    my $config        = $plugin->get_config_hash( 'blog:' . $entry_blog_id );
+    my $config        = $plugin->get_config_hash( 'blog:' . $blog->id );
     return if !$config->{supr_enable};
 
-    my $ctx             = $tmpl->context;
-    my $entry           = $ctx->stash('entry');
-    my $twitter_checked = "checked" if $config->{twitter_default};
-    my $fb_checked      = "checked" if $config->{fb_default};
-    my $supr_text       = ( $entry ? $entry->meta('supr_text') : '' );
-    my $supr_dis        = ( $supr_text ne '' ? 'disabled' : '' );
+    my ($entry,$posted_to,$posted_on,$supr_text);
+    my ($twitter_checked,$fb_checked);
+    if ($param->{id}) {
+	$entry = MT->model('entry')->load($param->{id});
+	$posted_to = join(' and ',split(',',$entry->supr_posted_to()));
+	$posted_on = relative_date( $entry->supr_posted_on, time, $entry->blog );
+	$supr_text = $entry->supr_text();
+    }
+    if ($entry && $posted_to) {
+	$twitter_checked = "checked" if $entry->supr_posted_to =~ /twitter/;
+	$fb_checked      = "checked" if $entry->supr_posted_to =~ /facebook/;
+    } else {
+	$twitter_checked = "checked" if $config->{twitter_default};
+	$fb_checked      = "checked" if $config->{fb_default};
+    }
+    my $supr_dis        = ( $supr_text ne '' ? 'disabled="disabled"' : '' );
 
     my $kw_field = $tmpl->getElementById('keywords')
       or return $app->error('cannot get the keywords block');
@@ -171,34 +186,112 @@ sub edit_entry_param {
         'app:setting',
         {
             id    => 'su_twitter',
+	    class => $supr_text ne '' ? "already-posted" : "",
             label => $app->translate('Post on Twitter & Facebook with Su.pr'),
         }
     ) or return $app->error('cannot create the su_twitter element');
+    my $postedHTML;
+    if ($posted_to) {
+	$postedHTML = "Posted to $posted_to " . ($posted_on =~ /ago/ ? "" : " on ") 
+	    . $posted_on;
+    }
     my $innerHTML = <<HTML;
 <script type="text/javascript">
 <!-- Begin
-function countChars(field,cntfield) {
-cntfield.value = 119 - field.value.length;
+function countChars() {
+  \$('.supr-controls .char-counter span').html(140 - \$('#su_twitter').val().length);
 }
-//  End -->
+\$(document).ready( function() {
+    \$('#su_twitter-field .posted a').click( function() {
+	\$('#su_twitter-field .posted').hide();
+	\$('#su_twitter-field .post').show();
+	\$('#su_twitter').removeAttr('disabled');
+	\$('#su_twitter-field').removeClass('already-posted');
+	\$(this).parent().find('input[name=supr_repost]').val(1);
+    });
+    countChars();
+});
+    \$.clipboardReady(function(){
+	$\( '#urls-field a.copy' ).click(function(){
+          var txt = \$(this).parent().find('span a').html();
+	  \$.clipboard( txt );
+          return false;
+	});
+    }, { swfpath: "<mt:StaticWebPath>plugins/Supr/jquery.clipboard.swf", debug: true } );
+// End -->
 </script>
-<textarea name="su_twitter" id="su_twitter" rows="2" cols="60" $supr_dis
-onKeyDown="countChars(document.entry_form.su_twitter,document.entry_form.twitlength)"
-onKeyUp="countChars(document.entry_form.su_twitter,document.entry_form.twitlength)">$supr_text</textarea>
+    <textarea name="su_twitter" id="su_twitter" rows="3" cols="60" $supr_dis
+      onKeyDown="countChars()" onKeyUp="countChars()">$supr_text</textarea>
     
-	<div class="supr-controls">
+    <div class="supr-controls">
       <div class="char-counter">
-        <input readonly type="text" name="twitlength" size="3" maxlength="3" value="119" /> characters left
+        <span>140</span> characters left
       </div>
-      <label class="twitter" for="supr-this"><input type="checkbox" name="twitter-this" $twitter_checked id="twitter-this" value="1"  /> Post this on Twitter</label>
-      <label class="fb" for="fb-this"><input type="checkbox" name="fb-this" $fb_checked id="fb-this" value="1"  /> Post this on Facebook</label>
-	  <p class="help">Twitter posts are a maximum of 140 characters; if your su.pr URL is appended to the end of your document, you have 119 characters available.</p>
-    </div>
+      <div class="post pkg">
+        <span>Post to:</span>
+        <label class="twitter" for="supr-this"><input type="checkbox" name="twitter-this" $twitter_checked id="twitter-this" value="1"  /> Twitter</label>
+        <label class="fb" for="fb-this"><input type="checkbox" name="fb-this" $fb_checked id="fb-this" value="1"  /> Facebook</label>
+      </div>
 HTML
+    if ($posted_on) {
+	$innerHTML .= <<HTML;
+      <div class="posted pkg">
+        <p>$postedHTML</p>
+        <input type="hidden" name="supr_repost" value="0" />
+        <a href="javascript:void(0)" id="supr_repost" onclick="repost()">repost</a>
+      </div>
+HTML
+    }
+    $innerHTML .= "    </div>";
     $su_field->innerHTML($innerHTML);
     $tmpl->insertAfter( $su_field, $kw_field )
       or return $app->error('failed to insertAfter.');
+
+    if ($entry) {
+	my $title_field = $tmpl->getElementById('title')
+	    or return $app->error('cannot get the title block');
+	my $urls_field = $tmpl->createElement(
+					      'app:setting',
+					      {
+						  id    => 'urls',
+						  show_label => 0,
+						  label => $app->translate('Post URLs'),
+					      }
+					      ) or return $app->error('cannot create the URLs element');
+	$innerHTML = "<ul>";
+	$innerHTML .= '<li class="pkg"><label>URL:</label><span><a href="'.$entry->permalink.'" target="_new">'.
+	    $entry->permalink.'</a></span><a class="copy" href="javascript:void(0)"><img src="<$mt:StaticWebPath$>plugins/Supr/copy.png" width="16" height="15" /></a></li>'
+	    if ($entry->status == MT->model('entry')->RELEASE());
+	$innerHTML .= '<li class="pkg"><label>Short URL:</label><span><a href="'.$entry->supr_url.'" target="_new">'.
+	    $entry->supr_url.'</a></span><a class="copy" href="javascript:void(0)"><img src="<$mt:StaticWebPath$>plugins/Supr/copy.png" width="16" height="15" /></a></li>'
+	    if ($entry->supr_url);
+	$innerHTML .= "</ul>";
+	
+	$urls_field->innerHTML($innerHTML);
+	$tmpl->insertAfter( $urls_field, $title_field )
+	    or return $app->error('failed to insertAfter.');
+    }
+
     $param;
+}
+
+sub _str_replace {
+    my $replace_this = shift;
+    my $with_this  = shift; 
+    my $string   = shift;
+
+    MT->log("Replacing $replace_this with $with_this");
+    
+    my $length = length($string);
+    my $target = length($replace_this);
+    
+    for(my $i=0; $i<$length - $target + 1; $i++) {
+	if(substr($string,$i,$target) eq $replace_this) {
+	    $string = substr($string,0,$i) . $with_this . substr($string,$i+$target);
+	    return $string; #Comment this if you what a global replace
+	}
+    }
+    return $string;
 }
 
 1;
